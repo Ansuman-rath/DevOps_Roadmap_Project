4c3941d6d4a782ee715542711d0e10a6a2a7917e
Ansuman-rath <ansumanr.linkedin@gmail.com>
2025-08-11 10:41:58 +0530

    Add files via upload

---
+++ DevOps/Node.js Service Deployment/roles/app/files/node-service/app.js
@@ -0,0 +1,11 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(80, () => {
+  console.log('Server is running on port 80');
+});
